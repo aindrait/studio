4c50f4f2f59b17e8d50173ca72658c9f07283628
Anton <aindra.go@gmail.com>
2025-07-30 02:54:38 +0000

    saat layar lebar, panel version sepertinya menyita tempat. apa perlu dir

--- src/components/documentation-viewer.tsx
+++ src/components/documentation-viewer.tsx
@@ -68,8 +68,8 @@ export function DocumentationViewer({ module }: DocumentationViewerProps) {
   }
 
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
-      <div className="lg:col-span-2 space-y-6">
+    <div className="grid grid-cols-1 lg:grid-cols-4 gap-8">
+      <div className="lg:col-span-3 space-y-6">
         <Card>
           <CardHeader>
             <CardTitle className="font-headline text-3xl">
